51203dc444230d1dc663eb5904c50cacf4b827cc
white.li <white.li@coherencedigital.com>
2024-11-08 12:50:50 +0800

    update layout

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: [
-        'resource2.heygen.ai',
-        'files2.heygen.ai',
-        // 添加其他需要的域名
+      'resource2.heygen.ai',
+      'files2.heygen.ai',
+      // 添加其他需要的域名
     ],
-},
+  },
+  eslint: {
+    dirs: ['src'], // 只在 src 目录下运行 ESLint
+    ignoreDuringBuilds: false, // 构建时不忽略 ESLint 错误
+  },
 };
 
 export default nextConfig;
